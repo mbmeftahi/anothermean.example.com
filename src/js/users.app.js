165748776016ed801c08b5f18506e95517440446
Mohammad B Meftahi <mmeftahi@hotmail.com>
2019-04-24 14:42:48 -0500

    user management views

--- src/js/users.app.js
+++ src/js/users.app.js
@@ -59,6 +59,124 @@ var usersApp = (function() {
       app.innerHTML = table;
     }
   }
+
+  function createUser(){
+    var app = document.getElementById('app');
+  
+    var form =  `
+        <div class="card">
+          <div class="card-header clearfix">
+            <h2 class="h3 float-left">Create a New User</h2>
+            <div class="float-right">
+              <a href="#" class="btn btn-primary">Cancel</a>
+            </div>
+          </div>
+          <div class="card-body">
+              <form id="createUser" class="card-body">
+              <div id="formMsg" class="alert alert-danger text-center">Your form has errors</div>
+  
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="first_name">First Name</label>
+                  <input type="text" id="first_name" name="first_name" class="form-control" required>
+                </div>
+  
+                <div class="form-group col-md-6">
+                  <label for="last_name">Last Name</label>
+                  <input type="text" id="last_name" name="last_name" class="form-control" required>
+                </div>
+              </div>
+  
+              <div class="row">
+                <div class="form-group col-md-6">
+                  <label for="username">Username</label>
+                  <input type="text" id="username" name="username" class="form-control" required>
+                </div>
+  
+                <div class="form-group col-md-6">
+                  <label for="email">Email</label>
+                  <input type="email" id="email" name="email" class="form-control" required>
+                </div>
+              </div>
+  
+              <div class="text-right">
+                <input type="submit" value="Submit" class="btn btn-lg btn-primary btn-sm-block">
+              </div>
+            </form>
+          </div>
+        </div>
+    `;
+  
+    app.innerHTML=form;
+  }
+
+  function viewUser(id){
+
+    let uri = `${window.location.origin}/api/users/${id}`;
+    let xhr = new XMLHttpRequest();
+    xhr.open('GET', uri);
+  
+    xhr.setRequestHeader(
+      'Content-Type',
+      'application/json; charset=UTF-8'
+    );
+  
+    xhr.send();
+  
+    xhr.onload = function(){
+      let app = document.getElementById('app');
+      let data = JSON.parse(xhr.response);
+      let card = '';
+  
+      card = `<div class="card">
+        <div class="card-header clearfix">
+          <h2 class="h3 float-left">${data.user.first_name} ${data.user.last_name}</h2>
+          <div class="float-right">
+            <a href="#edit-${data.user._id}" class="btn btn-primary">Edit</a>
+          </div>
+        </div>
+        <div class="card-body">
+          <div>${data.user.username}</div>
+          <div>${data.user.email}</div>
+        </div>
+      </div>`;
+  
+      app.innerHTML = card;
+    }
+  }
+
+  function postRequest(formId, url){
+    let form = document.getElementById(formId);
+    form.addEventListener('submit', function(e){
+      e.preventDefault();
+
+      let formData = new FormData(form);
+      let uri = `${window.location.origin}${url}`;
+      let xhr = new XMLHttpRequest();
+      xhr.open('POST', uri);
+
+      xhr.setRequestHeader(
+        'Content-Type',
+        'application/json; charset=UTF-8'
+      );
+
+      let object = {};
+      formData.forEach(function(value, key){
+        object[key]=value;
+      });
+
+      xhr.send(JSON.stringify(object));
+      xhr.onload = function(){
+        let data = JSON.parse(xhr.response);
+        if(data.success===true){
+          window.location.href = '/';
+        }else{
+          document.getElementById('formMsg').style.display='block';
+        }
+      }
+    });
+  }
+
   return {
     load: function(){
       let hash = window.location.hash;
@@ -66,11 +184,14 @@ var usersApp = (function() {
     
       switch(hashArray[0]){
         case '#create':
-          console.log('CREATE');
+          createUser();
+          postRequest('createUser', '/api/users');
+        //console.log('CREATE');
           break;
     
         case '#view':
-          console.log('VIEW');
+          viewUser(hashArray[1]);
+        //console.log('VIEW');
           break;
     
         case '#edit':
